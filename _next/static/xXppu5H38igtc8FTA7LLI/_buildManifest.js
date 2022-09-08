18b43686e65ccbcf21f3ecc78add757df337cc4b
sauravbanna <90598576+sauravbanna@users.noreply.github.com>
2022-09-08 03:56:00 +0000

    Deploying to gh-pages from @ sauravbanna/sauravbanna.github.io@939cdcd9eb8c99d9e272a7eb6bc998ce6ff5e659 🚀

---
+++ _next/static/xXppu5H38igtc8FTA7LLI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/155-969134b9095005e1.js","static/css/02ff8f9013d7b023.css","static/chunks/pages/index-b00713f9dc668728.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()

d44a268a0cc4cf0f00fe240c132605f7bee09c50
sauravbanna <90598576+sauravbanna@users.noreply.github.com>
2022-10-22 04:31:16 +0000

    Deploying to gh-pages from @ sauravbanna/sauravbanna.github.io@89a71a34d59a9bf5a7abf2c1e64e73604656cb8d 🚀

---
+++ _next/static/0NPDzC4F029EwsmyReH9h/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/155-6dd462f4d4e0088b.js","static/css/3bc438aacb351f8d.css","static/chunks/pages/index-a0ef69aac930ae3a.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/qUiNicfi_BG7uu_TelkcO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/155-6dd462f4d4e0088b.js","static/css/3bc438aacb351f8d.css","static/chunks/pages/index-dc50baa02ed0e109.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()

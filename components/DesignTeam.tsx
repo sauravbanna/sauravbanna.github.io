088310c127ac7a2386bbf38441b9924ba4da7a04
sauravbanna <sauravbanna@gmail.com>
2023-07-08 14:26:04 -0700

    added design team projects

--- components/DesignTeam.tsx
+++ components/DesignTeam.tsx
@@ -1,12 +1,26 @@
 import SectionHeader from "./SectionHeader/SectionHeader"
 import Typography from "@mui/material/Typography"
+import Grid from "@mui/material/Grid"
 import {DESIGN_TEAM_SECTION} from "../utils/constants"
+import styles from "../styles/Home.module.css"
+
 
 const DesignTeam = () => {
     return (
         <SectionHeader title="Design Team">
             <Typography variant="h6">
                 {DESIGN_TEAM_SECTION}
+                 <Grid container className={styles.projects}>
+                    {[].map((ele : IProjectData) => {
+                        return (
+                            <Grid item key={`ProjectGrid_${ele.title}`}>
+                                <Project
+                                    project={ele}
+                                />
+                            </Grid>
+                        );
+                    })}
+                </Grid>
             </Typography>
         </SectionHeader>
     );

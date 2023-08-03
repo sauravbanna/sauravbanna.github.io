a1b024c7c4316019af0ecfc1d0bd1405d6e9b9fb
sauravbanna <sauravbanna@gmail.com>
2023-08-02 19:07:10 -0700

    title and formatting

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,20 +3,27 @@ import NavBar from "../components/NavBar/NavBar"
 import Typography from "@mui/material/Typography"
 import AppTheme from "../styles/AppTheme"
 import { ThemeProvider } from '@mui/material/styles'
-
+import styles from "../styles/Home.module.css"
 
 
 const Home: NextPage = () => {
   return (
     <ThemeProvider theme={AppTheme}>
         <NavBar />
-        <div>
-            <Typography variant="h1">
+        <div
+            className={styles.pageTitle}
+            id="ProjectsPageHeader"
+        >
+            <Typography variant="h2">
                 Projects
             </Typography>
         </div>
-        <div>
-            placeholder
+        <div
+            className={styles.pageContent}
+        >
+            <Typography variant="h6">
+                Here are some of the projects I've worked on over the past few years!
+            </Typography>
         </div>
     </ThemeProvider>
   )
